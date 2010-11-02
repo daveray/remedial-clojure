a9a36176f50c7dc1aed7b9a4ffd009a97b9a42d4
daveray <daveray@gmail.com>
2010-11-02 14:54:53 -0400

    Simple compojure frontent

--- rhymetime/project.clj
+++ rhymetime/project.clj
@@ -1,7 +1,10 @@
 (defproject rhymetime "1.0.0-SNAPSHOT"
   :description "Rhyming dictionary example for remedial Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.5.2"]
+                 [hiccup "0.3.0"]
+                 [ring/ring-jetty-adapter "0.3.1"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
-  :main rhymetime.core)
+  :main rhymetime.web)
